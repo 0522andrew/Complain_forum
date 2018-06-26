550b616705d6797bc64dfab08b2d8ddc681e85cb
hope51607 <hope51607@yahoo.com.tw>
2018-06-26 22:00:58 +0800

    addPost

--- backend.js
+++ backend.js
@@ -1,17 +1,61 @@
 'use strict';
 
+class Blog{
+    constructor(obj){
+        this.content = obj.content;
+        this.time = obj.time;
+        this.author = obj.author;
+        this.name = obj.name;
+        this.messageCount = obj.messageCount || 0;
+        this.like = obj.like || 0;
+        this.dislike = obj.dislike || 0;
+    }
+    toString(){
+        return JSON.stringify(this);
+    }
+}
+
 class Bynorth{
     constructor(){
         LocalContractStorage.defineProperty(this,'blog_count',null)
+        LocalContractStorage.defineMapProperty(this,'blog',{
+            parse: function (text) {
+                obj=JSON.parse(text);
+                return new Blog(obj);
+            },
+            stringify: function (o) {
+                return o.toString();
+            }
+        })
     }
     init(){
         this.blog_count = 0;
-        console.log(this.blog_count);
     }
     test(){
-        this.blog_count++;
-        console.log(this.blog_count);
         return this.blog_count;
+    }
+    addPost(content,hash,name) {
+        let blogId = this.blog_count++;
+        let time = new Date();
+        newBlog=new Blog({
+            'content' : content,     //文章內容
+            'time': time,         //時間
+            'author': hash,       //作者地址
+            'name': name,         //作者暱稱
+            'messageCount': 0,  //留言數
+            'like': 0,          //like數
+            'dislike': 0        //dislike數
+        })
+        this.blog.put(blogId,newBlog);
+        var r={
+            'time': new Date(),
+            'blogId': blogId
+        }
+        console.log(r);
+        return r;
+    }
+    getPost(id){
+        return this.blog.get(id)
     }
 }
 module.exports = Bynorth;

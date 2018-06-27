1751e3c5ae46e7e3569b95a7f7c75157f5a2ba73
0522andrew <andrew4408@gmail.com>
2018-06-27 16:29:37 +0800

    blog presentation 50%

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
 $(document).ready(() => {
-    
-})
+    $(".thumb-up-pre").hover((event) => {
+        event.currentTarget.textContent = "👍";
+    }, (event) => {
+        event.currentTarget.textContent = "👍🏻";
+    });
+
+    $(".thumb-down-pre").hover((event) => {
+        event.currentTarget.textContent = "👎";
+    }, (event) => {
+        event.currentTarget.textContent = "👎🏻";
+    });
+});

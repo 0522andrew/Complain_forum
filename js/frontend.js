6b38d2f3da0e7246c9c26a5f5fd20c20e334f3ee
dd233 <80776877@qq.com>
2018-06-30 10:59:32 +0800

    qrcode disable

--- js/frontend.js
+++ js/frontend.js
@@ -17,6 +17,7 @@ neb.setRequest(new Nebulas.HttpRequest("https://testnet.nebulas.io"));
 var dappAddress = 'n1ovn4GPda3qnbk8AHXtvWWpwLkscttudzz'; // 合約地址
 var testUser = 'n1SZdYuB9kd6kpLBaCgxrPrV29175st5NVD';
 var serialNumber; //交易序列号
+var intervalQuery; //periodically query tx results
 
 var latestBlogsData;
 var hotBlogsData;
@@ -143,14 +144,26 @@ function initLatest() {
     });
 }
 
+var options = {
+    callback: NebPay.config.testnetUrl,   //交易查询服务器地址
+    listener: listenerFunction, //为浏览器插件指定listener,处理交易返回结果
+    qrcode: {
+		showQRCode: false,      //是否显示二维码信息
+		container: undefined,    //指定显示二维码的canvas容器，不指定则生成一个默认canvas
+		completeTip: undefined, // 完成支付提示
+		cancelTip: undefined // 取消支付提示
+	}
+};
+
 // nebpay call addPost
 function addPost(callArgs) {
     var to = dappAddress;
     var value = 0;
     var callFunction = "addPost";
    
-    serialNumber =  nebPay.call(to, value, callFunction, callArgs) 
+    serialNumber =  nebPay.call(to, value, callFunction, callArgs, options) 
 
+    nebPay.queryPayInfo(serialNumber, options).then()
     // var options = {callback: NebPay.config.testnetUrl}
     // nebPay.queryPayInfo(serialNumber, options) //search transaction result from server (result upload to server by app)
     //     .then(function (resp) {
@@ -165,20 +178,82 @@ function addMessage(callArgs) {
     var to = dappAddress;
     var value = 0;
     var callFunction = "addMessage";
-    console.log(1111111)
-    serialNumber =  nebPay.call(to, value, callFunction, callArgs) 
-    console.log(2222)
+    serialNumber =  nebPay.call(to, value, callFunction, callArgs, options); 
 
-    // var options = {callback: NebPay.config.testnetUrl}
-    // nebPay.queryPayInfo(serialNumber, options) //search transaction result from server (result upload to server by app)
-    //     .then(function (resp) {
-    //         console.log(resp)
-    //     })
-    //     .catch(function (err) {
-    //         console.log(err);
-    //     });
+    nebPay.queryPayInfo(serialNumber, options).then()
+
+     intervalQuery = setInterval(function() {
+            funcIntervalQuery();
+        }, 10000); //it's recommended that the query frequency is between 10-15s.
+
+}
+//Query the result of the transaction. queryPayInfo returns a Promise object.
+function funcIntervalQuery() {   
+    nebPay.queryPayInfo(serialNumber)   //search transaction result from server (result upload to server by app)
+        .then(function (resp) {
+            console.log("tx result: " + resp)   //resp is a JSON string
+            var respObject = JSON.parse(resp)
+            if(respObject.code === 0){
+                //The transaction is successful 
+                
+                clearInterval(intervalQuery)    //stop the periodically query 
+            }
+        })
+        .catch(function (err) {
+            console.log(err);
+        });
 }
 
+
+function listenerFunction(serialNumber,result){
+    console.log(`交易結果： ${serialNumber} is: ` + JSON.stringify(result))
+}
+
+function checkStatus(resp) {
+    console.log("监听");
+    if (resp == "Error: Transaction rejected by user"){
+        console.log(resp);
+        alert("初始化取消！");
+        return false;
+    } else {
+        console.log(resp);
+        console.log("已提交区块链网络，请等待写入区块链！");
+        checkPayStatus(resp.txhash);
+    }
+}
+
+
+function checkPayStatus(txhash) {
+    console.log("checkpaystatas "+txhash);
+
+    // $(".add_human_detail").hide();
+    // $(".show_human_detail").hide();
+    // $(".loading_div").show();
+    $.notify("Querying, please wait.")
+    var timerId = setInterval(function(){
+        nasApi.getTransactionReceipt({
+            hash:txhash
+        }).then(function(receipt){
+            console.log("checkPayStatus");
+            if(receipt.status == 1){
+                clearInterval(timerId);
+                var res = receipt.execute_result;
+                console.log("test success return="+res);
+
+                getBlog();
+
+            }else if(receipt.status == 0){
+                clearInterval(timerId);
+                console.log("test fail err="+receipt.execute_error);
+                alert("失败，请再次尝试！");
+
+                getBlog();
+            }
+        }).catch(function(err){
+            console.log("test error");
+        });
+    },3*1000);
+}
 
 // 獲取用戶地址
 function getUserAddress() {
